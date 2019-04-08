9f37b86838662d4dd628d69513228c8e9dd5dd26
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2019-04-09 00:35:34 +0300

    added a part of figure controller

--- src/sample/GamePanel.java
+++ src/sample/GamePanel.java
@@ -14,25 +14,25 @@ public class GamePanel extends Pane {
     private Tetrominos tetromino = new Tetrominos();
     private Timeline runGame;
     private final int AMOUNT_OF_BLOCKS = 14;
-    private int[][] glass = new int[AMOUNT_OF_BLOCKS + 1][10];
+    private int[][] glass = new int[AMOUNT_OF_BLOCKS + 1][9];
     private ArrayList<int[][]> tetrominos = new Tetrominos().setTetrominos();
 
     GamePanel() {
-        Arrays.fill(glass[AMOUNT_OF_BLOCKS], 1);
         setFocusTraversable(true);
+        Arrays.fill(glass[AMOUNT_OF_BLOCKS], 1);
     }
 
     public void run() {
         tetromino.initializeShape(tetrominos);
         tetromino.paint(this);
-        runGame = new Timeline(new KeyFrame(Duration.millis(300), event -> {
-            tetromino.stepDown();
-            if (tetromino.isOnTheFloor(glass)) {
+        runGame = new Timeline(new KeyFrame(Duration.millis(200), event -> {
+            if (tetromino.isTouchFloor(glass)) {
                 tetromino.leaveOnTheFloor(glass);
                 tetromino = new Tetrominos();
                 tetromino.initializeShape(tetrominos);
                 tetromino.paint(this);
             }
+            tetromino.stepDown();
         }));
         runGame.setCycleCount(Timeline.INDEFINITE);
         runGame.play();
@@ -42,20 +42,14 @@ public class GamePanel extends Pane {
         pane.setOnKeyPressed(event -> {
             switch (event.getCode()) {
                 case UP:
-                    System.out.println(true);
                     break;
                 case DOWN:
-                    System.out.println();
                     break;
                 case LEFT:
-                    tetromino.stepLeft();
+                    tetromino.stepLeft(glass, event.getCode());
                     break;
                 case RIGHT:
-                    System.out.println(true);
-                    tetromino.stepRight();
-                    break;
-                case SHIFT:
-                    System.out.println(true);
+                    tetromino.stepRight(glass, event.getCode());
                     break;
             }
         });
--- src/sample/Shape.java
+++ src/sample/Shape.java
@@ -1,8 +1,12 @@
 package sample;
 
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -22,16 +26,19 @@ public class Shape {
         }
     }
 
-    void stepLeft() {
-        for (Rectangle rectangle : figure) {
-            rectangle.setX(rectangle.getX() - BLOCK_SIZE);
+    void stepLeft(int[][] glass, KeyCode keyCode) {
+        if (!isTouchWall(glass, keyCode)) {
+            for (Rectangle rectangle : figure) {
+                rectangle.setX(rectangle.getX() - BLOCK_SIZE);
+            }
         }
-        System.out.println();
     }
 
-    void stepRight() {
-        for (Rectangle rectangle : figure) {
-            rectangle.setX(rectangle.getX() + BLOCK_SIZE);
+    void stepRight(int[][] glass, KeyCode keyCode) {
+        if (!isTouchWall(glass, keyCode)) {
+            for (Rectangle rectangle : figure) {
+                rectangle.setX(rectangle.getX() + BLOCK_SIZE);
+            }
         }
     }
 
@@ -46,7 +53,22 @@ public class Shape {
         }
     }
 
-    boolean isOnTheFloor(int[][] glass) {
+    private boolean isTouchWall(int[][] glass, KeyCode keyCode) {
+        for (Rectangle rectangle : figure) {
+            if (keyCode.equals(KeyCode.LEFT) && (rectangle.getX() == 0
+                    || glass[(int) (rectangle.getY() / BLOCK_SIZE)][(int) (rectangle.getX() / BLOCK_SIZE) - 1] == 1)) {
+                return true;
+            }
+            if (keyCode.equals(KeyCode.RIGHT) && (rectangle.getX() == 240
+                    || glass[(int) (rectangle.getY() / BLOCK_SIZE)][(int) (rectangle.getX() / BLOCK_SIZE) + 1] == 1)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    boolean isTouchFloor(int[][] glass) {
         for (Rectangle rectangle : figure) {
             if (glass[(int) rectangle.getY() / BLOCK_SIZE + 1][(int) rectangle.getX() / BLOCK_SIZE] > 0) {
                 return true;
@@ -63,6 +85,8 @@ public class Shape {
 
     void paint(Pane pane) {
         for (Rectangle rectangle : figure) {
+            Image image = new Image("file:images/greenBlock.png");
+            rectangle.setFill(new ImagePattern(image));
             pane.getChildren().add(rectangle);
         }
     }
